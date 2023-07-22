78151e454e04c19368691b4b122a2f4f165fe4b3
John <john.doros10@bcmail.cuny.edu>
2023-07-22 19:48:52 -0400

    Calculator-App

---
+++ CalculatorApp/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.calculator
+import com.example.calculator.ui.theme.CalculatorTheme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.unit.dp
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+import com.example.calculator.ui.theme.MediumGray
+
+@ExperimentalComposeUiApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(state = state, onAction = viewModel::onAction,buttonSpacing = buttonSpacing,modifier = Modifier.fillMaxSize().background(MediumGray).padding(16.dp))
+            }
+        }
+    }
+}
+
